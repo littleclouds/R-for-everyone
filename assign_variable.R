bbbf500510a7376ec58f7d7df1010a11ce49c968
Little clouds <36187672+littleclouds@users.noreply.github.com>
2020-05-11 07:48:37 +0530

    Add files via upload

---
+++ assign_variable.R
@@ -0,0 +1,10 @@
+a <- 2
+b <- -1
+c <- -4
+print(a)
+
+#solving quadratic equation
+solution1 <- (-b + sqrt(b^2 - 4*a*c))/(2*a) #negative value
+solution2 <- (-b - sqrt(b^2 - 4*a*c))/(2*a) #plus value
+print(solution1)
+print(solution2)
